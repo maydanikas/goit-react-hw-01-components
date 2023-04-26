1cff5ccfc36a68d3ef0473a98544d16348610884
maydanikas <123328755+maydanikas@users.noreply.github.com>
2023-04-26 16:53:49 +0200

    task 4 complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import user from '../constants/user.json';
 import data from '../constants/data.json';
 import friends from '../constants/friends.json';
+import transactions from '../constants/transactions.json';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 // console.log(friends);
 
@@ -19,7 +21,8 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />;
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
