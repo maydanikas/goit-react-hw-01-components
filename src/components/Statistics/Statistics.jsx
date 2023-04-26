19ea76ff6f242b71d1409d069b9853ee74ff6ea3
maydanikas <123328755+maydanikas@users.noreply.github.com>
2023-04-26 07:54:30 +0200

    unusing variable removed

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,10 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
-  const color = getRandomHexColor();
   //   console.log(title);
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
-
       <ul className={css.statList}>
         {stats.map(({ id, label, percentage }) => (
           <li
